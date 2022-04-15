14feba154c3878dd0cc8cc4c4803ed7fc984180c
Taisei Fujisawa <taisei.mrchil@gmail.com>
2022-04-15 16:26:53 +0900

    コメント追加

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -1,7 +1,15 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
+# save a flatten vector as a png
 def mnist_show(flattenpixel):
+    """
+    Args: 
+        flattenpixel: a flatten vector of one image
+    Returns:
+        None
+    """
+
     fig = plt.figure()
     plt.imshow(flattenpixel.reshape(28, 28), cmap='Greys')
     #plt.show()
@@ -10,15 +18,17 @@ def mnist_show(flattenpixel):
 
 def main():
     RANDOM_SEED = 0
-    FIG_NO = 0
+    FIG_NO = 0      # an index number of the image saved as a png file and showed
     tf.random.set_seed(RANDOM_SEED)
 
+    # load test data
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
     x_train = x_train/255
     x_test = x_test/255
 
+    # create a NN model
     model = tf.keras.Sequential([
-        tf.keras.layers.Reshape((28, 28, 1), input_shape=(28, 28)),
+        tf.keras.layers.Reshape((28, 28, 1), input_shape=(28, 28)),     # redundant but needed
         tf.keras.layers.Conv2D(32, (3, 3), activation='relu'\
             , input_shape=(28, 28)),
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu', name="conv"),
@@ -33,19 +43,23 @@ def main():
 
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
+    # leaning
     history = model.fit(x_train, y_train, batch_size=128, epochs=20, validation_split=0.2)
     model.save("my_model")
 
+    # evaluation
     test_loss, test_acc = model.evaluate(x_test, y_test, verbose=0)
     print('Test loss:', test_loss)
     print('Test accuracy:', test_acc)
 
+    # prediction
     predictions = model.predict(x_test)
     print(f"the prediction is {predictions[FIG_NO].argmax()}.")
     print(f"the answer is {y_test[FIG_NO]}.")
     print("the input image has been stored as \"Sample.png\"")
     mnist_show(x_test[FIG_NO])
 
+    # show the result of prediction
     loss     = history.history['loss']
     val_loss = history.history['val_loss']
     nb_epoch = len(loss)
